0aca95e41a89969721a2e31989f1a7daaab67c0f
Lukas Crockett <lukas.crockett@gmail.com>
2024-03-20 23:29:39 -0600

    Implemented VFO control

--- IC-720a.new/IC-720a.new.ino
+++ IC-720a.new/IC-720a.new.ino
@@ -16,10 +16,14 @@
 //Modes enum
 enum modes {AM, LSB, USB, CW, RTTY};
 
+//VFO A/B
+enum vfos {VFOA, VFOB};
+
 /*
  * Write data to the radio
  * As specified by ICOM documentaion, data is placed on
  * bus, then DBC is pulsed for 50ms
+ * finally, RT is pulsed to trigger radio side data validation (not checked)
  */
 void writeData(byte data) {
     /*PORTF (analog pins) layout
@@ -46,11 +50,19 @@ void writeData(byte data) {
     //Remove data from bus
     PORTF = B000000;
 
+    delay(10);
+
+    //Set RT high
+    PORTB |= RT_HIGH;
+    delay(10);
+
+    PORTB ^= RT_HIGH; 
+
 }
 
 
 /*
- * Set the radio mode 
+ * Set the2 radio mode 
  */
 void writeMode(modes mode) {
     switch (mode) {
@@ -74,6 +86,54 @@ void writeMode(modes mode) {
     }
 }
 
+/*
+ * Write the VFO Data & mode
+ * freq: integer of desired frequency in hz
+ */
+void writeVFO(unsigned long freq, modes mode,  vfos vfo) {
+    //0xE represents HF band (only option on IC-720a)
+   writeData(0xE);
+
+    delay(2);
+
+    //Write mode data
+    writeMode(mode);
+
+    //Choose vfo a or b
+    switch (vfo) {
+        case VFOA:
+            writeData(0xA);
+            break;
+        case VFOB:
+            writeData(0xB);
+            break;
+    
+    }
+    
+    //split frequency into digits: 1Mhz, 100k, 10k, 1k, 100
+    //Digit is stored in array index 0-5 in descending order
+    byte digits[5];
+    digits[0] =  freq / 1000000; //EX 14,087,300 -> 14
+    digits[1] =  (freq % 1000000) / 100000; // 14,087,300 -> 0
+    digits[2] =  (freq % 100000) / 10000; // 14,087,300 -> 8
+    digits[3] =  (freq % 10000) / 1000; // 14,087,300 -> 7
+    digits[4] =  (freq % 1000) / 100; // 14,087,300 -> 3
+    
+    //digits[0] = freq >> 6 & 0xF;
+    Serial.print("\nfreq value: ");
+    Serial.println(freq, DEC);
+    //Write freq data
+    int i = 0;
+    for (i = 0; i < 5; i++) {
+        Serial.print("digit index ");
+        Serial.println(i, DEC);
+        Serial.println(digits[i], DEC);
+        writeData(digits[i]);
+        delay(25);
+    }
+}
+
+
 
 void setup() {
     //Setup control pins (pins 8-11)
@@ -93,5 +153,7 @@ void setup() {
 
 
 void loop() {
-
+    Serial.println("writing freq");
+    writeVFO(14087300, USB, VFOA);
+    delay(1000);
 }
